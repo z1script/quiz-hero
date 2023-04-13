e691c8faa3da28a72525f4d052bf2a8fe358a23d
Zihadul Islam <zynscriptofficial@gmail.com>
2023-04-13 06:32:17 +0600

    quiz not show bug fixed

--- index.js
+++ index.js
@@ -46,9 +46,9 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
-  quizData = data;
-  displayQuiz(data);
+  const data = await res.json();
+  const quizData = data;
+  displayQuiz(quizData);
 };
 
 // Displaying quiz on quiz page
@@ -59,6 +59,7 @@ const displayQuiz = (data) => {
   }
 
   data.forEach((quiz, i) => {
+    console.log(quiz);
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
@@ -74,7 +75,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
